625071ee71d56ff0cd3c4e137b9d5e520805e8ce
Jimmy Béland-Bédard <jbeland@armrecouvrement.com>
2017-09-26 13:15:06 -0400

    Adding click event

--- dev/js/selectpicker.js
+++ dev/js/selectpicker.js
@@ -25,9 +25,9 @@ var selectpicker = {
 
     initSelectPicker: function() {
         document.querySelectorAll('.selectpicker').forEach(function(picker) {
-            var pickerParentElement = picker.parentNode;
             var divElement = document.createElement('div');
-            selectpicker.createSelectpickerButton(picker, pickerParentElement, divElement);
+
+            selectpicker.createSelectpickerButton(picker, divElement);
             selectpicker.displayedValue(picker, divElement);
 
             divElement.innerHTML += '<div class="dropdown-menu-select"><ul class="select-option-list"></ul></div>';
@@ -38,6 +38,7 @@ var selectpicker = {
             });
 
             divElement.appendChild(picker);
+            selectpicker.setSelectOptionEvent();
         });
     },
 
@@ -47,10 +48,11 @@ var selectpicker = {
         });
     },
 
-    createSelectpickerButton: function (picker, pickerParentElement, divElement) {
+    createSelectpickerButton: function (picker, divElement) {
+        var pickerParentElement = picker.parentNode;
+
         pickerParentElement.replaceChild(divElement, picker);
         divElement.classList.add('selectpicker-group');
-        selectPickerTitle = picker.getAttribute('title');
         divElement.innerHTML = '<button id="selectpickerButton" class="" role="button"> <span class="selectpicker-value"></span> <span class="caret"><span></button>';
         divElement.querySelector('button#selectpickerButton').setAttribute('data-type', picker.getAttribute('data-type'));
         divElement.querySelector('button#selectpickerButton').classList.add('selectpicker-button');
@@ -58,10 +60,37 @@ var selectpicker = {
 
     displayedValue: function (picker, divElement) {
         if (selectpicker.hasValueSelected(picker)) {
-            divElement.querySelector('button.selectpicker-button > span.selectpicker-value').innerHTML = picker.getAttribute('title');
+
         } else {
-            divElement.querySelector('button.selectpicker-button > span.selectpicker-value').innerHTML = picker.getAttribute('placeholder');
-            divElement.querySelector('button.selectpicker-button > span.selectpicker-value').classList.add('placeholder');
+            if(picker.getAttribute('placeholder')) {
+                divElement.querySelector('button.selectpicker-button > span.selectpicker-value').innerHTML = picker.getAttribute('placeholder');
+                divElement.querySelector('button.selectpicker-button > span.selectpicker-value').classList.add('placeholder');
+            } else if(picker.getAttribute('title')) {
+                divElement.querySelector('button.selectpicker-button > span.selectpicker-value').innerHTML = picker.getAttribute('title');
+                divElement.querySelector('button.selectpicker-button > span.selectpicker-value').classList.add('title');
+            }
         }
+    },
+
+    setSelectOptionEvent: function() {
+        var selectedValue = '';
+        document.querySelectorAll('div.selectpicker-group > div.dropdown-menu-select > ul.select-option-list > li').forEach(function(option) {
+            option.addEventListener('click', function() {
+                option.setAttribute('selected', '');
+                selectedValue = '' + option.value;
+                option.parentElement.parentElement.parentElement.querySelectorAll('select.selectpicker > option').forEach(function(selectOption) {
+                    if(selectOption.value == option.value) {
+                        selectOption.setAttribute('selected', '');
+                    } else {
+                        selectOption.removeAttribute('selected');
+                    }
+                });
+                document.querySelectorAll('div.selectpicker-group > div.dropdown-menu-select > ul.select-option-list > li').forEach(function (option) {
+                    if ('' + option.value !== selectedValue) {
+                        option.removeAttribute('selected');
+                    }
+                });
+            })
+        });
     }
 }
